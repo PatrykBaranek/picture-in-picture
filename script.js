8a1124462576170274027af0c4d4be175f6439d1
Patryk Baranek <cien2460045@gmail.com>
2020-10-21 00:45:31 +0200

    modify script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+
+async function selectMediaStream() {
+    try {
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// On Load
+selectMediaStream();
